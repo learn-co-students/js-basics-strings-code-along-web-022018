198bd9a2eaadf71542d1e95710a19d8e0f1f5e44
bafab <bafa@tutanota.de>
2018-04-06 17:14:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const currentUser = 'Grace Hopper'
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
-const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
-const shortGreeting = `Welcome, ${currentUser.substring(0,1)}!`
+let currentUser = 'bob'
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+let shortGreeting = `Welcome, ${currentUser[0]}!`
